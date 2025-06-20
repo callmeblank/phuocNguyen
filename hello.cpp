3688036d980869fc9ed6561a40eb8474a9cbbfaa
callmeblank <themanwhosenameisblanking@gmail.com>
2025-06-20 14:41:45 +0000

    the king

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,5 @@ int main(){
     std::string name;
     std::cout << "Enter your name here: ";
     std:: cin >> name;
-    std::cout << "hello " + name;;
+    std::cout << "hello " << name << ". I am the king";
 }
